5bced85613ffb72ca1330b36befd1a92ac7bbe0f
Rakagali Resda Krisandi Putra <faesalgali@gmail.com>
2026-04-13 13:42:43 +0700

    model annd migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Enums\UserRole;
 
-#[Fillable(['name', 'email', 'password'])]
+
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -22,11 +26,53 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'email_verified_at',
+        'remember_token',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => UserRole::class,
         ];
+    }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return match ($panel->getId()) {
+            'admin'     => $this->role === UserRole::admin,
+            'developer' => $this->role === UserRole::developer,
+            'tester'    => $this->role === UserRole::tester,
+            default     => false,
+        };
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === UserRole::admin;
+    }
+
+    public function isDev()
+    {
+        return $this->role === UserRole::developer;
+    }
+
+    public function isTester()
+    {
+        return $this->role === UserRole::tester;
     }
 }
